d43948d542c37bd0e408a33afcb74808da54a910
Julius Angeles <juls.angeles18@gmail.com>
2019-04-22 23:27:06 +0800

    Refactored to Leaflet

--- src/components/MapDashboard.vue
+++ src/components/MapDashboard.vue
@@ -1,32 +1,57 @@
 <template>
     <div id="map-container">
-        <GmapMap :center="{lat:	16.416665, lng:120.5999976}"
-                :zoom="7"
-                map-type-id="roadmap"
-                style="width: 100%; height: 600px">
-                  <GmapMarker
-                    :key="index"
-                    v-for="(m, index) in markers"
-                    :position="m.position"
-                    :clickable="true"
-                    :draggable="true"
-                    @click="center=m.position"
-                />
-        </GmapMap>
+        <l-map :worldCopyJump="true" :maxBounds="bounds" :minZoom="2.3" :zoom="zoom" :center="center">
+            <l-tile-layer :url="url"></l-tile-layer>
+            <l-marker @mouseover="openPopup"
+            :key="index"
+            v-for="(mark, index) in markers"
+            :lat-lng="latLng(mark.lat, mark.lng)"></l-marker>
+        </l-map>
     </div>
 </template>
+
 <script>
-import { gmapApi } from 'vue2-google-maps';
+import { LMap, LTileLayer, LMarker } from 'vue2-leaflet';
+import L from 'leaflet';
+import mockResponse from '../mockResponse.json';
 
 export default {
     name: 'map-dashboard',
     props: [],
-    computed: {
-        google: gmapApi
+    components: {
+     LMap, LTileLayer, LMarker 
+    },
+    computed : {
+        markers() {
+            return mockResponse;
+        }
+    },
+    data() {
+        return {
+            zoom:2.3,
+            center: L.latLng(0, 0),
+            url:'https://tile.thunderforest.com/cycle/{z}/{x}/{y}.png?apikey=bbafaa3b53534ea0b7b19a553e22af1d',
+            bounds : new L.LatLngBounds(new L.LatLng(89.99346179538875, 180), new L.LatLng(-89.98155760646617, -180)),
+            msg: 'This is just a test data'
+        }       
+    },
+    created : function() {
+    },
+    methods : {
+        latLng : function(lat, lng) {
+            return L.latLng(lat, lng);
+        },
+        openPopup : function(event) {
+            event.target.bindTooltip(this.msg, {direction : 'top', permanent : false});
+        }
     }
     
 }
 </script>
 <style scoped>
+#map-container {
+    height: 95vh;
+    width:100%;
+}
 
 </style>
